75e3c1c3871dc42353bfaf86864915501d0149da
j0ppi <corybesson0514@gmail.com>
2026-08-11 22:49:34 +0200

    Fix disponibilité

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Les variables Supabase sont manquantes."
+  );
+}
+
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  supabaseUrl,
+  supabaseAnonKey
 );
