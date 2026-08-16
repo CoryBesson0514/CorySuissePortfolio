81d03614eaaf3aa3d13307ec39293a75fdfca910
j0ppi <corybesson0514@gmail.com>
2026-08-16 20:53:46 +0200

    added loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingScreen from "./components/LoadingScreen";
+
+export default function Loading() {
+  return <LoadingScreen show={true} />;
+}
+
+// =========================
+// Loading est utilisé par Next.js pour utiliser/appliquer loadingscreen.tsx
+// =========================
